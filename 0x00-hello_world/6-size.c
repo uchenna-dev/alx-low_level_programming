8b0e2a792b8ea7a825d27fa1dc4869c1b6fb31d7
uchenna-dev <meacessories.store@gmail.com>
2022-04-09 17:47:12 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ long long int lli;
 float f;
 printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: 4 byte(s)\n", (int)sizeof(i));
-printf("Size of a long int: 4 byte(s)\n", (long int)sizeof(li));
+printf("Size of a long int: 8 byte(s)\n", (long int)sizeof(li));
 printf("Size of a long long int: 8 byte(s)\n", (long long int)sizeof(lli));
 printf("Size of a float: 4 byte(s)\n", (float)sizeof(f));
 return (0);
