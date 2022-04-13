881f4bd07d3a770ef6fbe102140cc6c3218662f9
uchenna-dev <meacessories.store@gmail.com>
2022-04-13 16:22:07 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,11 +14,10 @@ int count;
 count = 0;
 while (count < 10)
 {
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{	
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
 _putchar(alphabet);
 }
-
 
 count++;
 _putchar('\n');
