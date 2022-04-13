4ce4d310c2c897d000da7584585ea7e668561f95
uchenna-dev <meacessories.store@gmail.com>
2022-04-12 18:36:20 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,17 +5,19 @@
  *
  * Return: Always 0.
  */
+void times_table(void)
+{
 int a;
 int b;
 int c;
-for (a =0; a <= 9; a++)
+for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
 {
 c = a * b;
-if ((c / 100 == 0)
+if ((c / 10 == 0)
 {
-if (b ==0)
+if (b == 0)
 {
 _putchar ('0');
 }
