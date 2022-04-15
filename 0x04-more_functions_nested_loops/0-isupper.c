b9f76899dd0975828cdd5ce546320189692b72ab
uchenna-dev <meacessories.store@gmail.com>
2022-04-15 07:45:06 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,11 @@
 #include "main.h"
 /**
- * _isupper - checkes upper case
- * @c: print int
- * Return: give 1 or 0
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
-int _isupper(int c)
+int _isupper(int letter)
 {
-if (c >= 'A' && c <= '2')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+return (letter >= 'A' && letter <= 'Z');
 }
