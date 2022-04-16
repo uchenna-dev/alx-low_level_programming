592870519ebc393edcc3c67b7521a10c4030af8a
uchenna-dev <meacessories.store@gmail.com>
2022-04-16 03:32:57 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,18 @@
 
 void more_numbers(void)
 {
-	int c, x;
+int c, x;
 
-	for (c = 0; c < 10; c++)
-        {
-		for (x = 0; x <= 14; x++)
-		{
-			if (x > 9)
-			{
-				_putchar((x / 10) + 48);
-			}
-			_putchar((x % 10) + 48);
-		}
-		_putchar(10);
-	}
+for (c = 0; c < 10; c++)
+{
+for (x = 0; x <= 14; x++)
+{
+if (x > 9)
+{
+_putchar((x / 10) + 48);
+}
+_putchar((x % 10) + 48);
+}
+_putchar(10);
+}
 }
