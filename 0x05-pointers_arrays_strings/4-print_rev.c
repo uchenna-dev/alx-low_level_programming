5a0a5f212d5aa4796d96a0802784d9a7abc21b7b
uchenna-dev <meacessories.store@gmail.com>
2022-04-21 22:46:23 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+/**
+ * print_rev - print a string
+ * @s: pointer char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i, n;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+n = i - 1;
+for (i = n; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+return;
+}
+
+
+
+
+
