4105348b4a6c6a381a745a48e7678e119ea01f60
uchenna-dev <meacessories.store@gmail.com>
2022-04-21 21:53:22 +0100

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts - print a string
+ * @str: pointer char
+ * Return: void
+ */
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return;
+}
+
