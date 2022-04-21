111114b5a8715a9f48207257ac98498ef990e6fd
uchenna-dev <meacessories.store@gmail.com>
2022-04-21 20:59:15 +0100

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - length of string
+ * @s: char
+ * Return: int
+ */
+int _strlen(char *s)
+{
+int i; 
+
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+return (i);
+}
+
