04f7d644135667bdb4f3ba19df014b7beb4e63ff
uchenna-dev <meacessories.store@gmail.com>
2022-04-21 22:51:07 +0100

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,27 +1,16 @@
-#include "stdio.h"
+#include <stdio.h>
 /**
- * print_rev - print a string
- * @s: pointer char
+ * _puts - print a string
+ * @str: pointer char
  * Return: void
  */
-void print_rev(char *s)
+void _puts(char *str)
 {
-int i, n;
-
-for (i = 0; s[i] != '\0'; i++)
+int i;
+for (i = 0; str[i] != '\0'; i++)
 {
-continue;
-}
-n = i - 1;
-for (i = n; i >= 0; i--)
-{
-_putchar(s[i]);
+putchar(str[i]);
 }
 _putchar('\n');
 return;
 }
-
-
-
-
-
