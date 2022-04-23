e5d215df350420fceda0bb0d1f094d8ddbf40fdc
uchenna-dev <meacessories.store@gmail.com>
2022-04-23 17:23:48 +0100

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,15 +8,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
+int i, j;
 i = 0;
-while (i < n && src[i] != '\0')
+while (dest[i] != '\0')
 {
-dest[i] = src[i];
 i++;
 }
-while (i < n)
+j = 0;
+while (j < n && src[j] != '\0')
 {
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
 dest[i] = '\0';
 i++;
 }
