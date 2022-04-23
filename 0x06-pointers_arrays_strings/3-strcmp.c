163e04a549d9db469a0eb89e17c67bd717095dd2
uchenna-dev <meacessories.store@gmail.com>
2022-04-23 18:37:07 +0100

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 /**
  * _strcmp - compares two strings
  * @s1: A pointer to a character that will be changed
- * @s2: A pointer to a character that will also be changed/ modified/updated
+ * @s2: A pointer to a character that will also be changed/modified/updated
  * Return: dest
  */
 int _strcmp(char *s1, char *s2)
