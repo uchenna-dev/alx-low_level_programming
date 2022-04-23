5c88cf4bfc6740c9cfd256ca0d8023f8e984ee96
uchenna-dev <meacessories.store@gmail.com>
2022-04-23 01:34:35 +0100

    task11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * pointers tasks
+ */
+int main(void)
+{
+int num;
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}
+
+
