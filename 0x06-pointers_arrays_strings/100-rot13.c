4f0304e06c50a914b2d72114dacddeef6a3ae715
uchenna-dev <meacessories.store@gmail.com>
2022-04-23 23:06:07 +0100

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *rot13 - function that encodes string using rot13
+ * @s: string
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+
+int x, y;
+
+char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (x = 0; s[x] != '\0'; x++)
+{
+
+for (y = 0; input[y] != '\0'; y++)
+{
+
+if (s[x] == input[y])
+{
+
+s[x] = output[y];
+break;
+}
+}
+}
+
+return (s);
+}
