74de2423a72b867d000632be215820b4c5c12a72
uchenna-dev <meacessories.store@gmail.com>
2022-04-26 20:45:01 +0100

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _memcpy - copies memory area
  * @dest: destination memory area
@@ -7,14 +8,22 @@
  *
  * Return: memory area replaced
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 unsigned int a;
 
 for (a = 0; a < n; a++)
+
 {
+
 dest[a] = src[a];
+
 }
+
  return (dest);
+
 }
+
 
