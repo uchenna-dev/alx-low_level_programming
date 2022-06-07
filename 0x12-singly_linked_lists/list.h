d68d43438a66eacb37a53444da0ed3c8dba55124
Empress <101710164+uchenna-dev@users.noreply.github.com>
2022-06-07 14:00:30 +0100

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -3,7 +3,7 @@
 
 /*
  * File: lists.h
- * Auth: ike victoria uche
+ * Auth: Ukonu Divine Chisom
  * Desc: Header file containing prototypes and definitions for all functions
  *       and types written in the 0x11-singly_linked_lists directory.
  */
