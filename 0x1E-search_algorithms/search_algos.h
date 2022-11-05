0437c572e010b8a0883c5e509280e0f5539cda65
uchenna-dev <meacessories.stores@gmail.com>
2022-11-05 21:14:21 +0100

    search and readme

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+/** HEADERS*/
+[D[A[C[C[C[C[C[C[C[C[C[C[C  */
+
+#include <stdio.h>
+#include <stdio.h>
+#include <math.h>
+
+/** Prototype */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+#endif
